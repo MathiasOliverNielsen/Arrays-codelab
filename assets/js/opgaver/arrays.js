577d49c18a3862b14a7a53354b30e23d939117d2
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-08-25 13:33:38 +0200

    nåede ikke opgave 2 rød

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -11,7 +11,14 @@ function addExtra(extra) {
 }
 
 function updateExtras() {
-  myExtrasListElement.innerHTML = myExtras.map((extra) => `<li>${extra}</li>`).join('');
+  let html = '';
+  myExtras.forEach(function (extra) {
+    html += `<li>${extra}</li>`;
+  });
+  myExtrasListElement.innerHTML = html;
 }
 
 // opgave 2 console.logs
+
+// Byg videre på opgave 1.
+// Du skal lave en dropdown, der inderholder de valg brugeren har taget. hvis man vælger fra denne nye dropdown, fjerner man det valgte element.
